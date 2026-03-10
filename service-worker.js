d5512664b85d7b8681b7e642940c5e263bac6ebd
yakashi0111 <yutaka.akashi@moegi.waseda.jp>
2026-03-10 19:07:26 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'sqotify-v1';
+const FILES = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
